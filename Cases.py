072f9b5b8c77e078ccc884f4ae5afac655eeb6c4
S Perera <sperera1922@gmail.com>
2025-12-12 17:55:56 +0530

    Print the given text in different cases.

---
+++ Cases.py
@@ -0,0 +1,9 @@
+# Given text = "Hello" , print it in different cases.
+
+text = "Hello"
+
+print(text.upper())        # HELLO
+print(text.capitalize())   # Hello
+print(text.lower())        # hello
+print(text.title())        # Hello
+print(text.swapcase())     # hELLO
